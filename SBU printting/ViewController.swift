c0b69336481310cae69b7a575c8d3c40509d204f
samuelhoffmann <samuel.hoffmann@stonybrook.edu>
2019-01-27 16:10:42 -0500

    Initial Commit

---
+++ SBU printting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SBU printting
+//
+//  Created by Samuel Hoffmann on 1/27/19.
+//  Copyright © 2019 Samuel Hoffmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
